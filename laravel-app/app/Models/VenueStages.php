753df3fd616d1c8aa75cca0fd54dff36b266a47b
Logan <logan.voss@logicalposition.com>
2022-07-28 22:01:18 -0500

    create migrations and models

---
+++ laravel-app/app/Models/VenueStages.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VenueStages extends Model
+{
+    use HasFactory;
+}
---
+++ laravel-app/database/migrations/2022_07_28_233055_create_venue_stages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venue_stages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('venue_id');
+            $table->text('stage_plot');
+            $table->text('sound_person_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venue_stages');
+    }
+};
